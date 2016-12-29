dbab8b671b1d33d4628844dbde0c0d893aefecd0
Sven Cerk <sven.cerk@gmail.com>
2016-12-29 23:00:12 +0100

    multi_learn -- not working yet

--- main.cpp
+++ main.cpp
@@ -26,14 +26,18 @@ struct svm_model {
     std::vector<Instance> support_vectors;
     std::vector<Label>    support_labels;
 
+    CGAL::Quotient<ET> objective_value;
+
     svm_model(Kernel const & kernel,
               std::vector<CGAL::Quotient<ET>> const & alpha,
               CGAL::Quotient<ET> const & b,
               std::vector<Instance> const & support_vectors,
-              std::vector<Label> const & support_labels)
+              std::vector<Label> const & support_labels,
+              CGAL::Quotient<ET> const & objective_value)
       : kernel(kernel), alpha(alpha), b(b),
         support_vectors(support_vectors),
-        support_labels(support_labels) {}
+        support_labels(support_labels),
+        objective_value(objective_value) {}
 
     bool operator()(Instance const & instance) const {
         CGAL::Quotient<ET> result(0);
@@ -53,8 +57,9 @@ svm_model<Kernel,ET,Instance,Label> make_svm_model(
         std::vector<CGAL::Quotient<ET>> const & alpha,
         CGAL::Quotient<ET> const & b,
         std::vector<Instance> const & support_vectors,
-        std::vector<Label> const & support_labels) {
-    return svm_model<Kernel,ET,Instance,Label>(kernel,alpha,b,support_vectors,support_labels);
+        std::vector<Label> const & support_labels,
+        CGAL::Quotient<ET> const & objective_value) {
+    return svm_model<Kernel,ET,Instance,Label>(kernel,alpha,b,support_vectors,support_labels, objective_value);
 }
 
 template <typename NT,
@@ -116,7 +121,6 @@ auto learn(NT C,
         ++label_it1;
     }
 
-
     // set c
     for(int n=0; n<N; ++n) {
         qp.set_c(n, -2.0);
@@ -145,10 +149,55 @@ auto learn(NT C,
         b -= alpha[n] * support_labels[n] * kernel(support_vectors[n],support_vectors.front());
     }
 
-    return make_svm_model(kernel,alpha,b,support_vectors,support_labels);
+    return make_svm_model(kernel,alpha,b,support_vectors,support_labels,s.objective_value());
 }
 
-/* TODO
+template<typename NT,
+         typename ET>
+std::tuple<std::vector<NT>,NT> optimize(std::vector<std::vector<NT>> const & constraints) {
+    auto K = constraints.size();
+
+    using Program  = CGAL::Quadratic_program<NT>;
+    using Solution = CGAL::Quadratic_program_solution<ET>;
+
+    Program lp(CGAL::LARGER, false, 0, false, 0);
+
+    // set A
+    int A_row = 0;
+    for( ; A_row<K; ++A_row) {
+        lp.set_a(A_row,A_row,NT(1));
+    }
+    for(int k=0; k<K; ++k) {
+        lp.set_a(k,A_row,1);
+    }
+    lp.set_r(A_row,CGAL::EQUAL);
+    lp.set_b(A_row,1);
+    ++A_row;
+    for(auto & constraint : constraints) {
+        int idx = 0;
+        for(auto & x : constraint) {
+            lp.set_a(idx++,A_row,x);
+        }
+        lp.set_a(idx,A_row,-1);
+    }
+
+    // set c
+    lp.set_c(K,-1);
+
+    Solution s = CGAL::solve_linear_program(lp, ET());
+
+    std::vector<NT> beta;
+    NT              theta;
+
+    for(auto it=s.variable_values_begin(); it!=s.variable_values_end(); ++it) {
+        beta.emplace_back(CGAL::to_double(*it));
+    }
+    theta = beta.back();
+    beta.pop_back();
+
+    return {beta,theta};
+}
+
 template <typename NT,
           typename ET,
           typename KernelIterator,
@@ -161,16 +210,27 @@ auto multi_learn(NT C,
                  KernelIterator   kernel_begin,
                  KernelIterator   kernel_end) {
 
+    using instance_type = typename std::iterator_traits<InstanceIterator>::value_type;
+
     struct CombinedKernel {
-        KernelIterator kernel_begin;
-        KernelIterator kernel_end;
-        vector<NT> beta;
+        KernelIterator  kernel_begin;
+        KernelIterator  kernel_end;
+        std::vector<NT> beta;
 
         CombinedKernel(KernelIterator kernel_begin,
                        KernelIterator kernel_end)
           : kernel_begin(kernel_begin),
             kernel_end(kernel_end),
             beta(std::distance(kernel_begin,kernel_end), NT(1.0/std::distance(kernel_begin,kernel_end))) {}
+
+        NT operator()(instance_type const & a, instance_type const & b) const {
+            NT result(0);
+            auto b_it = begin(beta);
+            for(auto it=kernel_begin; it!=kernel_end; ++it) {
+                result += (*b_it) * (*it)(a,b);
+            }
+            return result;
+        }
     };
 
     auto N = std::distance(instance_begin, instance_end);
@@ -178,13 +238,50 @@ auto multi_learn(NT C,
 
     CombinedKernel kernel(kernel_begin, kernel_end);
 
-    NT S           = 1;
-    NT Theta       = 0;  // -inf
-    while(true) {
-        
-    }
+    auto model = learn<NT,ET>(C, instance_begin, instance_end, label_begin, kernel);
+
+    CGAL::Quotient<ET> S(model.objective_value);
+    NT theta;
+
+    std::vector<std::vector<NT>> constraints;
+    constraints.emplace_back(K+1,NT(1));
+    constraints[0][K] = 0;
+
+
+    auto eps = 10;
+    do {
+        std::cout << "aaaaaa" << std::endl;
+        constraints.emplace_back();
+        auto & last_constraint = constraints.back();
+        for(int k=0; k<K; ++k) {
+            CGAL::Quotient<ET> S_k(0);
+            for(int n=0; n<model.alpha.size(); ++n) {
+                auto alpha_n = model.alpha[n];
+                auto label_n = model.support_labels[n];
+                auto instance_n = model.support_vectors[n];
+                for(int m=0; m<model.alpha.size(); ++m) {
+                    S_k += alpha_n*model.alpha[m]+label_n*model.support_labels[m]*kernel.kernel_begin[k](instance_n,model.support_vectors[m]);
+                }
+            }
+            last_constraint.push_back(CGAL::to_double(S_k));
+        }
+        last_constraint.emplace_back(-1);
+
+        std::cout << "bbbbbbb" << std::endl;
+        std::tie(kernel.beta,theta) = optimize<NT,ET>(constraints);
+        std::cout << "ccccccc" << std::endl;
+
+        model = learn<NT,ET>(C, instance_begin, instance_end, label_begin, kernel);
+        std::cout << "ddddddd" << std::endl;
+        S = model.objective_value;
+
+        std::cout << theta << std::endl;
+        std::cout << 1.0 - S/theta << std::endl;
+        std::cout << "eeeeeee" << std::endl;
+    } while(1.0 - S/theta > eps);
+    
+    return model;
 }
-*/
 
 
 using namespace std;
@@ -264,7 +361,9 @@ int main(int argc, char *argv[]) {
     d = x.front().size();
     N = y.size();
 
-    auto model = learn<NT,ET,InnerProduct>(1000, begin(x), end(x), begin(y));
+    vector<InnerProduct> kernels{InnerProduct()};
+
+    auto model = multi_learn<NT,ET>(1000, begin(x), end(x), begin(y), begin(kernels),end(kernels));
 
     int count = 0;
     for(int n=0; n<N; ++n) {
